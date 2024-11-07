ca6ea0537e8d73ac1024f04a49750ce1841c3089
Rio Prayoga <yoprayogap@gmail.com>
2024-11-07 20:34:49 +0700

    feat: hero and install shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,15 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <>
-      <Navbar />
-      HOME
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
     </>
   );
 }
