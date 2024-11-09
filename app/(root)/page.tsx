89bc520376be25722ee9135ed9298ba59c1d81eb
Rio Prayoga <yoprayogap@gmail.com>
2024-11-09 09:34:08 +0700

    feat: add searchform and searchformreset

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
-import Navbar from "../components/Navbar";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -11,6 +17,8 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
